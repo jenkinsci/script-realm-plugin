9c6b91f336d9f7fa112c4403a6a7750e9d0d40c2
nicobo <nicobo@nicolabs.net>
2015-04-26 02:26:18 +0200

    + groovy snippet to delete all users from local Jenkins (for tests)

---
+++ src/test/resources/DeleteAllUsers.groovy
@@ -0,0 +1,17 @@
+import hudson.security.*
+import jenkins.security.*
+
+//
+// USE ONLY ON LOCAL JENKINS FOR TESTS !!!
+// DELETES ALL USERS FROM JENKINS !!!!
+//
+
+for ( userInfo in Jenkins.instance.getPeople().users ) {
+    User user = userInfo.getUser()
+    print("Deleting user "+user)
+    user.delete()
+}
+
+for ( userInfo in Jenkins.instance.getPeople().users ) {
+    println(userInfo.getUser())
+}
